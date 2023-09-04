88e10553dc5754a101edbb6853c420f894222382
smitaBL <smita.gaikwad@bridgelabz.com>
2023-09-04 14:12:29 +0530

    [smita]Added:req validation

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,20 +1,16 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
-import { newUserValidator } from '../validators/user.validator';
+import { newUserValidator, loginDetailvalidator } from '../validators/user.validator';
 import { userAuth } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
 
 //route to create a new user
-router.post('', userController.registerNewUser);
+router.post('', newUserValidator, userController.registerNewUser);
 
 //route to create a new user
-router.post('/login', userController.userLogin);
-
-
-
-
+router.post('/login', loginDetailvalidator, userController.userLogin);
 
 
 export default router;
--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -2,8 +2,24 @@ import Joi from '@hapi/joi';
 
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
-    fName: Joi.string().min(4).required(),
-    lName: Joi.string().min(4).required(),
+    firstName: Joi.string().min(4).required(),
+    lastName: Joi.string().min(4).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+  });
+  const { error, value } = schema.validate(req.body);
+  if (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: `${error}`
+    });
+  } else {
+    next();
+  }
+};
+
+export const loginDetailvalidator = (req, res, next) => {
+  const schema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required()
   });
