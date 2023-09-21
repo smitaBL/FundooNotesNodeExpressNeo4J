ff92c1f1b6a6b24dea5962d10ff71283b7168fca
smitaBL <smita.gaikwad@bridgelabz.com>
2023-09-21 17:00:52 +0530

    [smita]Added: secret key

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,7 @@
 import HttpStatus from 'http-status-codes';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 /**
  * Middleware to authenticate if user has a valid Authorization token
@@ -19,7 +21,7 @@ export const userAuth = async (req, res, next) => {
       };
     bearerToken = bearerToken.split(' ')[1];
 
-    const user = await jwt.verify(bearerToken, 'your-secret-key');
+    const user = await jwt.verify(bearerToken, process.env.SECRET_KEY);
     req.body.userId = user.email
     next();
   } catch (error) {
