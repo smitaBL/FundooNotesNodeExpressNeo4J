e6a25a9e025be87821bf24687835e82929a3dd50
smitaBL <smita.gaikwad@bridgelabz.com>
2023-09-28 17:59:58 +0530

    [smita]Added: swagger doc

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from "./swagger/swagger.json"
 
 import routes from './routes';
 
@@ -27,6 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('combined', { stream: logStream }));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(`/api/${api_version}`, routes());
 app.use(appErrorHandler);
